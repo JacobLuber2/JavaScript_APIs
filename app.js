2dc0d70e6f1e5cdf47e1d7b913408fd0ee697b35
JacobLuber2 <jaluber10@gmail.com>
2024-03-29 21:06:03 -0600

    finished

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-console.log("Hello World!\n==========\n");
-
+"use strict";
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+let submitBtn = document.getElementById("submitSearch");
+let searchInput = document.getElementById("searchWord");
+let img = document.getElementById("img");
+let feedbackP = document.getElementById("feedback");
+
+// Exercise 2 Section
+submitBtn.addEventListener("click", () => {
+fetch(`https://api.giphy.com/v1/gifs/translate?api_key=q2wNzQw66SYVj551f2A4uBhHBewUhf2z&s=${searchInput.value}`)
+.then((res) => {
+return res.json();
+})
+.then((res) => {
+img.src = res.data.images.original.url;
+})
+.catch((err) => {
+console.error(err);
+feedbackP.textContent = err;
+})
+.finally(() => {
+searchInput.value = "";
+})
+})
+
